652c21d6664d16d960bcf29d2633e59f33b0a70d
Volifter <4546202-volifter@users.noreply.gitlab.com>
2022-12-01 10:22:30 +0100

    Complete Day01

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -2,18 +2,23 @@ package day01
 
 import utils.*
 
+fun getElvesCalories(input: List<String>): List<Int> =
+    (input + listOf("")).fold(Pair(listOf<Int>(), 0)) { (cals, n), line ->
+        if (line.isEmpty())
+            Pair(cals + listOf(n), 0)
+        else
+            Pair(cals, n + line.toInt())
+    }.first
+
+fun part1(input: List<String>): Int = getElvesCalories(input).maxOf { it }
+
+fun part2(input: List<String>): Int =
+    getElvesCalories(input).sortedDescending().take(3).sum()
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    expect(part1(testInput), 0)
+    expect(part1(testInput), 24000)
+    expect(part2(testInput), 45000)
 
     val input = readInput("Day01")
     println(part1(input))
